c5b7e9dfcfdf94dc8aaeec5057cd6b3d012a9446
Andreu Villaró Marimon <andreuvillaro12@gmail.com>
2025-11-15 14:17:19 +0100

    Btn btn-x

---
+++ src/components/common/Button.vue
@@ -0,0 +1,16 @@
+<template>
+    <button v-bind="$attrs" @click="$emit('click', $event)">
+        <LucideLoader v-if="loading" class="w-4 h-4 animate-spin" />
+        <slot v-else></slot>
+    </button>
+</template>
+
+<script setup>
+    import { LucideLoader } from 'lucide-vue-next'
+
+    const props = defineProps({
+        loading: { type: Boolean, default: false },
+    })
+
+    defineEmits(['click'])
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,16 @@
 <template>
     <ChessBoard interactive />
+    <Button class="btn btn-create" @click="loading = !loading" :loading="loading">
+        <Trash class="w-5 h-5" />
+        Test del boto
+    </Button>
 </template>
 
 <script setup>
+    import { ref } from 'vue'
     import ChessBoard from '@/components/ChessBoard.vue'
+    import Button from '@/components/common/Button.vue'
+    import { Trash } from 'lucide-vue-next'
+
+    const loading = ref(false)
 </script>
