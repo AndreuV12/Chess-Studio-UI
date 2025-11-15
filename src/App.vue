824f88041e958086a33c4d947aefe4a330e0677b
Andreu Villaró Marimon <andreuvillaro12@gmail.com>
2025-11-15 12:36:01 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <template>
-    <router-view />
+    <router-view></router-view>
+    <ErrorModal v-model="ui.errorModal.show" :title="ui.errorModal.title" :message="ui.errorModal.message" />
+    <ActionModal
+        v-model="ui.actionModal.show"
+        :title="ui.actionModal.title"
+        :message="ui.actionModal.message"
+        :loading="ui.actionModal.loading"
+        :type="ui.actionModal.type"
+        @confirm="ui.actionModal.confirm"
+    />
+    <Toast
+        v-model="ui.toast.show"
+        :message="ui.toast.message"
+        :type="ui.toast.type"
+        :duration="ui.toast.duration"
+    ></Toast>
 </template>
 
 <script setup>
-// Nada más necesario aquí
+    import ErrorModal from '@/components/common/ErrorModal.vue'
+    import ActionModal from '@/components/common/ActionModal.vue'
+    import Toast from '@/components/common/Toast.vue'
+    import { useUiStore } from '@/stores/ui'
+    const ui = useUiStore()
 </script>
-
-<style>
-/* Estilos globales opcionales */
-</style>
